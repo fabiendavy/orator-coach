9cdc6cfe16a8000d571f4e24023e8f2c1cc2367e
Benjamin Teste <benjamin.teste@gmail.com>
2020-02-25 13:16:36 +0100

    start creation of pages_controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get "/stats", to: "pages#stats"
   get "/library", to: "pages#library"
 
-
   resources :sessions, only: [ :new, :create, :show ]
   resources :observers, only: [ :new, :create ] do
     resources :reviews, only: [ :new, :create ] do
