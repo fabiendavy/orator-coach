bd1bf46bbfa0de7725a2986d1dbca33ab41f5086
Mathieu Guillermit <mguillermit@gmail.com>
2020-02-25 11:40:34 +0100

    created all routes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :observer
-  belongs_to :session
+  belongs_to :session, through: :observer
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,5 @@
 class Session < ApplicationRecord
   belongs_to :speaker, class_name: 'User', foreign_key: 'user_id'
-
-  has_many :reviews
+  has_many :reviews, through: :observer
   has_many :observers
 end
