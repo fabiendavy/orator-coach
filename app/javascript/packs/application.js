a45937872fcbcf25d05edbaed26c71ea73d89c44
Benjamin Teste <benjamin.teste@gmail.com>
2020-02-26 12:51:13 +0100

    Start implementation of stats chart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("chartkick")
+require("chart.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,3 +24,4 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 import "bootstrap";
+import { myChart } from "../plugins/init_chart";
