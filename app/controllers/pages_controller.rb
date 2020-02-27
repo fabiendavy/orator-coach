e3e9cd89ed24a6d71c65fdc7c7e00ce37ae1023c
Benjamin Teste <benjamin.teste@gmail.com>
2020-02-27 12:27:38 +0100

    Get data from DB to supply to stats view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+  before_action :set_recordings, only: [ :get_comments_received,
+                                         :get_reviews_type ]
+
   def home
   end
 
@@ -13,6 +16,7 @@ class PagesController < ApplicationController
     @recordings_number_observer = Observer.where(user: current_user).count
     @comments_received_number = get_comments_received
     @comments_given_number = get_comments_given
+    @reviews_type_count = count_reviews_type
   end
 
   def library
@@ -21,19 +25,44 @@ class PagesController < ApplicationController
 
   private
 
+  def set_recordings
+    Recording.where(speaker: current_user)
+  end
+
   def get_comments_received
-    recordings = Recording.where(speaker: current_user)
-    recordings.sum { |recording| recording.reviews.count }
+    set_recordings.sum { |recording| recording.reviews.count }
   end
 
   def get_comments_given
     observers = Observer.where(user: current_user)
+
     observers.sum do |observer|
       observer.final_review? ? observer.reviews.count + 1 : observer.reviews.count
     end
   end
 
   def get_reviews_type
+    reviews_type = []
 
+    set_recordings.each do |recording|
+      recording.reviews.each { |review| reviews_type << review.review_type.parameterize }
+    end
+
+    reviews_type
+  end
+
+  def count_reviews_type
+    reviews_count = {}
+    reviews_type = get_reviews_type
+
+    reviews_type.each do |review_type|
+      if reviews_count.has_key?(review_type)
+        reviews_count[review_type] += 1
+      else
+        reviews_count[review_type] = 1
+      end
+    end
+
+    return reviews_count
   end
 end
