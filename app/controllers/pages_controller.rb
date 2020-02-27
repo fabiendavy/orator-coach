985a70b2a55fac563b529a0b9d21f2c4c6f75255
Benjamin Teste <benjamin.teste@gmail.com>
2020-02-27 11:36:49 +0100

    Get stats from db to display on stats view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,10 +9,31 @@ class PagesController < ApplicationController
   end
 
   def stats
-    @users = User.all
+    @recordings_number_speaker = Recording.where(speaker: current_user).count
+    @recordings_number_observer = Observer.where(user: current_user).count
+    @comments_received_number = get_comments_received
+    @comments_given_number = get_comments_given
   end
 
   def library
+
+  end
+
+  private
+
+  def get_comments_received
+    recordings = Recording.where(speaker: current_user)
+    recordings.sum { |recording| recording.reviews.count }
+  end
+
+  def get_comments_given
+    observers = Observer.where(user: current_user)
+    observers.sum do |observer|
+      observer.final_review? ? observer.reviews.count + 1 : observer.reviews.count
+    end
+  end
+
+  def get_reviews_type
 
   end
 end
