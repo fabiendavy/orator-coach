0bc4238ec185dc8dda344c944f1b5300f2ca7776
Nicolas Lebot <nicolas.lebot@yahoo.fr>
2020-02-28 14:13:39 +0100

    video start time

--- app/javascript/plugins/init_reviews.js
+++ app/javascript/plugins/init_reviews.js
@@ -5,17 +5,19 @@ const initReviews = () => {
   recordButton.addEventListener("click", (event) => {
     const recordHit = new Date();
     const startTime = recordHit.getTime();
+    console.log(startTime);
     return startTime;
   })
-  let sessionReviews = [];
-  reviewButton.forEach( button => button.addEventListener("click", (event) => {
-    const comment = event["toElement"].value;
-    const timeSent = new Date();
-    const commentTime = timeSent.getTime() - startTime;
-    const review = [comment, commentTime];
-    sessionReviews.push(review);
-    return sessionReviews;
-  }));
+  // let sessionReviews = [];
+  // reviewButton.forEach( button => button.addEventListener("click", (event) => {
+  //   const comment = event["toElement"].value;
+  //   const timeSent = new Date();
+  //   const commentTime = timeSent.getTime();
+  //   const review = [comment, commentTime];
+  //   sessionReviews.push(review);
+  //   console.log(sessionReviews);
+  //   return sessionReviews;
+  // }));
 }
 
 export { initReviews };
