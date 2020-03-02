79d454c2c33c3e11a4f841b2cec01b27b1e3362d
Fabien Davy <davyfabien@gmail.com>
2020-03-02 14:21:14 +0100

    add src to video

---
+++ app/javascript/packs/displayVideo.js
@@ -0,0 +1,15 @@
+const displayVideo = () => {
+  const videoDiv = document.getElementById('video-display');
+  const videos = document.querySelectorAll('.video');
+
+  if (videoDiv && videos) {
+    videos.forEach((video) => {
+      video.addEventListener('click', (event) => {
+        const url = event.currentTarget.dataset.url;
+        videoDiv.src = `https://res.cloudinary.com/fabdv/video/upload/${url}.webm`;
+      });
+    });
+  }
+};
+
+export { displayVideo };
