f7fb056e9d5448dc727a999bb1e33b415782cec9
Mathieu Guillermit <mguillermit@gmail.com>
2020-03-02 15:22:02 +0100

    wip on comments

--- app/javascript/plugins/init_comments.js
+++ app/javascript/plugins/init_comments.js
@@ -6,15 +6,6 @@ const initComment = () => {
   videos.forEach((item) => {
     item.addEventListener("click", (event) => {
       const recordingId = item.dataset.recordingid;
-      console.log (recordingId);
-
-      $.ajax({
-              url : "/dashboard",
-              type : "post",
-              data : { data_value: JSON.stringify(recordingId) }
-          });
-
-
     });
   });
 
