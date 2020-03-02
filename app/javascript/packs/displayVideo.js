be9396a2366c55c1eb746010976ec8a1e5501053
Fabien Davy <davyfabien@gmail.com>
2020-03-02 16:06:17 +0100

    repaired cl_video_tag

--- app/javascript/packs/displayVideo.js
+++ app/javascript/packs/displayVideo.js
@@ -1,20 +1,14 @@
 const displayVideo = () => {
   const videoDiv = document.getElementById('video-display');
   const videos = document.querySelectorAll('.video');
-  console.log(videoDiv);
-  console.log(videos);
 
   if (videos) {
-  console.log("Salut les moches");
     videos.forEach((video) => {
       video.addEventListener('click', (event) => {
-        console.log(event);
-        const url = event.currentTarget.dataset.url;
+        // const url = event.currentTarget.dataset.url;
         const recordingId = video.dataset.recordingid;
-        console.log(recordingId);
-        videoDiv.src = `https://res.cloudinary.com/fabdv/video/upload/${url}.webm`;
-        console.log(`http://localhost:3000/dashboard?recording_id=${recordingId}`);
-        window.location.replace(`http://localhost:3000/dashboard?recording_id=${recordingId}`);
+        window.location.replace(`/dashboard?recording_id=${recordingId}`);
+        // videoDiv.src = `https://res.cloudinary.com/fabdv/video/upload/${url}.webm`;
       });
     });
   }
