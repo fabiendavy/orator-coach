96ed27961b31aac712491b89c1adcb758ae9481b
Mathieu Guillermit <mguillermit@gmail.com>
2020-03-03 17:06:06 +0100

    animated comments on the right of the video

--- app/javascript/packs/displayVideo.js
+++ app/javascript/packs/displayVideo.js
@@ -2,7 +2,7 @@ const displayVideo = () => {
   const videoDiv = document.getElementById('video-display');
   const videos = document.querySelectorAll('.video');
 
-  if (videos) {
+  if (videos && videoDiv) {
     const cld = cloudinary.Cloudinary.new({ cloud_name: "my-cloud", secure: true});
     const reviewsType = document.querySelectorAll('.review-type');
     const reviewsTimestamp = document.querySelectorAll('.review-timestamp');
@@ -12,12 +12,27 @@ const displayVideo = () => {
       recordingTimestamp = parseInt(recordingTimestamp.value) / 1000;
     }
 
+    // Method to convert seconds in min:sec format
+    const timeConverter = (seconds) => {
+      let min = Math.floor(seconds / 60);
+      let sec = seconds - (min * 60)
+      let time;
+      if (sec < 10) {
+        time = `${min}:0${sec}`
+      } else {
+        time = `${min}:${sec}`
+      }
+      return time
+    }
+
+
     // console.log(reviewsType);
     const timestamps = [];
     const types = [];
     reviewsTimestamp.forEach((review) => {
       timestamps.push(Math.floor(parseInt(review.innerText) - recordingTimestamp));
     });
+
     reviewsType.forEach((review) => {
       types.push(review.innerText);
     });
@@ -27,11 +42,11 @@ const displayVideo = () => {
       // console.log(event.eventData.time);
       timestamps.forEach((item, index) => {
         if (event.eventData.time === item) {
-          const html = `<div class="review"><span class="review-type">${types[index]}</span> <span class="review-timestamp">00:0${item}</span></div>`;
+          const html = `<div class="review"><span class="review-type">${types[index]}</span> <span class="review-timestamp">${timeConverter(item)}</span></div>`;
           commentsDiv.insertAdjacentHTML('beforeend', html);
-        } 
+        }
       });
-    })  
+    })
 
     videos.forEach((video) => {
       video.addEventListener('click', (event) => {
