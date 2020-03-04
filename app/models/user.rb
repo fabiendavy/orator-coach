bad30e8dc4ec6010c43fd380f12d35607908774a
Fabien Davy <davyfabien@gmail.com>
2020-03-04 10:32:20 +0100

    change the sign in, sign up and display the first name of observers in dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :recordings
+  validates :first_name, presence: true
 end
